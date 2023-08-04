ddb4eb9f0aee3a49ba53657a4d9a1cacc650871e
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-04 11:58:08 +0700

    Delete api-vue directory

--- api-vue/api/api.js
+++
@@ -1,8 +0,0 @@
-// Apiการจัดการแบบรวมศูนย์
-const { http } = uni.$u
-
-// อินเทอร์เฟซทั่วไปสำหรับแอปเพล็ต
-export const wxLogin = code => http.get('/wxLogin/' + code)
-export const wxGetUserInfo = params => http.post('/wxGetUserInfo', params)
-export const wxGetPhoneNumber = (params, config = {}) =>
-  http.post('/wxGetPhoneNumber', params, (config = {}))
